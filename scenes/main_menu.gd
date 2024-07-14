7802b28480392f8aca509e2f459b8f2bbab97400
Diego Michel Castro <diegoalejandromichel95@gmail.com>
2024-07-13 18:16:26 -0600

    Agregado de menú principal

---
+++ scenes/main_menu.gd
@@ -0,0 +1,21 @@
+class_name MainMenu
+
+extends Control
+
+@onready var start_buton = $MarginContainer/HBoxContainer/VBoxContainer/Start_Button as Button
+@onready var exit_buton = $MarginContainer/HBoxContainer/VBoxContainer/Exit_Button as Button
+@onready var start_level = preload("res://main.tscn") as PackedScene
+
+
+func _ready():
+	start_buton.button_down.connect(on_start_pressed)
+	exit_buton.button_down.connect(on_exit_pressed)
+	
+	
+func on_start_pressed() -> void:
+	get_tree().change_scene_to_packed(start_level)
+	
+
+func on_exit_pressed() -> void:
+	get_tree().quit()
+	
